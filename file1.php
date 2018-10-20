73dfecb3f5b319a04d40c57df4148d4d0572249c
maratsimon <44301007+maratsimon@users.noreply.github.com>
2018-10-20 10:47:22 +0300

    Add files via upload

---
+++ file1.php
@@ -0,0 +1,57 @@
+
+<!DOCTYPE>
+<html>
+
+<head>
+    
+ <title>file1</title>   
+    
+</head>
+
+<body>
+    <pre>
+ <?php ?> 
+ 
+<?php print_r($_GET)?> <br> 
+
+<?php  $Name=$_GET["Name"]; 
+       $Position=$_GET['Position']; 
+    
+ echo $Name; echo $Position;   
+
+
+    
+?>    
+    
+ </pre>   
+  
+    
+    
+</body>
+
+
+
+
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
